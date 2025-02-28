d6b264b65245f1ce81a321886d24543bfe378bf3
angemiJR <111733781+angemiJR@users.noreply.github.com>
2025-02-28 10:25:03 +0100

    something

--- src/assets/pages/Hello.jsx
+++ src/assets/pages/Hello.jsx
@@ -1,5 +1,6 @@
 import Navbar from '../components/Navbar';
 import picture1 from '../pictures/Hello_1.svg';
+import World from '../pictures/World.svg';
 import '../styles/Hello.css';
 
 
@@ -10,10 +11,18 @@ import "../styles/Hello.css";
 function Hello() {
     return (
         <div>
-             <Navbar /> {/* Fixed navigation at the top */}
-             <div className='container'>
-          <img src={picture1} alt="" id='picture_hello' />
-        </div>
+            <Navbar /> {/* Fixed navigation at the top */}
+            <div className='container'>
+
+
+                <div className="hello_text">
+                    <h2>Hello</h2>
+                    <img src={World} alt="picture2" id='world' />
+
+                </div>
+
+                <img src={picture1} alt="picture" id='picture_hello' />
+            </div>
 
         </div>
     );
