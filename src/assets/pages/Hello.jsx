93171fe6fcc90eb39f13c6daa9532a8e900a4675
angemiJR <111733781+angemiJR@users.noreply.github.com>
2025-03-05 08:30:52 +0100

    settings

--- src/assets/pages/Hello.jsx
+++ src/assets/pages/Hello.jsx
@@ -1,31 +1,24 @@
 import Navbar from '../components/Navbar';
 import picture1 from '../pictures/Hello_1.svg';
-import World from '../pictures/World.svg';
+
 import '../styles/Hello.css';
 
 
 
 
-import "../styles/Hello.css";
+
 
 function Hello() {
     return (
-        <div>
+        <>
             <Navbar /> {/* Fixed navigation at the top */}
-            <div className='container'>
-
-
-                <div className="hello_text">
-                    <h2>Hello</h2>
-                    <img src={World} alt="picture2" id='world' />
-
-                </div>
-
-                <img src={picture1} alt="picture" id='picture_hello' />
+            <div className="container">
+                <img src={picture1} alt="Background" id="picture_hello" />
             </div>
-
-        </div>
+            </>
     );
 }
 
 export default Hello;
+
+
