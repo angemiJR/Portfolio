3328c795f83fbc29b2e6f10812bcd8cdf5f283ab
angemiJR <111733781+angemiJR@users.noreply.github.com>
2025-03-13 13:14:47 +0100

    slider works

--- src/assets/components/Work_chapter.jsx
+++ src/assets/components/Work_chapter.jsx
@@ -1,11 +1,24 @@
 
 import '../styles/Common.css';
+import '../styles/Work_chapter.css';
 
-function Work_chapter() {
+function Work_chapter(props) {
     return (
      <>
      
-     
+     <div className="Works_container">
+
+     <img src={props.img} alt={props.altText} className="picture" />
+     <h3 className="tittle">{props.tittle}</h3>
+     <p className="built_on">{props.built_on} <b> </b> </p>
+     <p className="note">{props.note} <i></i></p>
+
+
+
+
+
+
+     </div>
      
      
      
--- src/assets/pages/Work.jsx
+++ src/assets/pages/Work.jsx
@@ -4,24 +4,67 @@ import '../styles/Common.css';
 import Work_chapter from '../components/Work_chapter.jsx';
 import Chapter from '../components/Chapter_right.jsx';
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { EffectCoverflow, Pagination, Navigation } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
 const chapterText = "My work";
-function Work() {
 
-   
+function Work() {
     return (
         <>
             <div className='chapter_right'>
-
                 <Chapter text={chapterText} />
-
-            </div>
-            <div className='container5'>
-               <Work_chapter/>
             </div>
 
+            <div className="container5">
+            <div className='carousel-container'>
+                <Swiper
+                    effect="coverflow"
+                    grabCursor={false}
+                    centeredSlides={true}
+                    slidesPerView="auto"
+                    coverflowEffect={{
+                        rotate: 50,
+                        stretch: 0,
+                        depth: 100,
+                        modifier: 1,
+                        scale: 1,
+                        slideShadows: true,
+                    }}
+                    pagination={{
+                        clickable: true,
+                        type: "bullets",
+                        dynamicBullets: true,
+                        dynamicMainBullets: 3
+                    }}
+                    
+                    navigation={true}
+                    modules={[EffectCoverflow, Pagination, Navigation]}
+                    className="swiper-container"
+                >
+                    <SwiperSlide>
+                        <Work_chapter />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <Work_chapter />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <Work_chapter />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <Work_chapter />
+                    </SwiperSlide>
+                </Swiper>
+            </div>
+
+            </div>
         </>
     );
 }
 
-export default Work; // Ensure you have this default export
+export default Work;
