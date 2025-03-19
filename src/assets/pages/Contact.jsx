0161eb2c30fc72b5d492097ba48cfe38f7b205c7
angemiJR <111733781+angemiJR@users.noreply.github.com>
2025-03-19 10:23:55 +0100

    contact update

--- src/assets/pages/Contact.jsx
+++ src/assets/pages/Contact.jsx
@@ -1,7 +1,13 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
+
+
 import '../styles/Contact.css';
 import '../styles/Common.css';
 import Chapter from '../components/Chapter_left.jsx';
 import picture1 from '../pictures/Contact_me.svg';
+
 
 const chapterText = "Contact";
 
@@ -16,18 +22,35 @@ function Contact() {
             <div className="container6">
 
                 <div className="contact_text_box">
-                    <h6>Thank you for visiting my website!
+                    <h6>Thank you for visiting my website!</h6>
 
-Feel free to reach out—I’m always open to new opportunities, collaborations, and exciting projects.
+                    <h6>Feel free to reach out—I’m always open to new opportunities, collaborations, and exciting projects.</h6>
 
-With a background in graphic engineering and design, I have a deep understanding of the entire creative, production, and workflow process. From initial concepts to final execution, I know how to bridge the gap between design and development. As a Scrum Master, I ensure smooth workflows, efficient team collaboration, and agile project execution. Combining these skills with front-end development, I bring designs to life with precision, ensuring both functionality and aesthetics work seamlessly together.
+                    <h6>With a background in graphic engineering and design, I have a deep understanding of the entire creative, production, and workflow process. From initial concepts to final execution, I know how to bridge the gap between design and development. As a Scrum Master, I ensure smooth workflows, efficient team collaboration, and agile project execution. Combining these skills with front-end development, I bring designs to life with precision, ensuring both functionality and aesthetics work seamlessly together. </h6>
 
-Let’s connect and create something amazing!</h6>
+                    <h6>Let’s connect and create something amazing!</h6>
                 </div>
-                  <img src={picture1} alt="Background" id="picture_contact" />
-            
+                <img src={picture1} alt="Background" id="picture_contact" />
+
             </div>
-            <div className="line"></div>
+            <footer>
+                <div className="line"></div>
+
+                <div className="footer_icons">
+                    <p>Created by Emilija Miladinovic 2025</p>
+
+                    <a href="href=https://github.com/angemiJR">
+                        <FontAwesomeIcon icon={faGithub} className="icons" />
+                    </a>
+                    <a href="mailto:angemi.jr@gmail.com">
+                        <FontAwesomeIcon icon={faEnvelope} className="icons" />
+                    </a>
+                    <a href="https://www.linkedin.com/in/emilija-miladinovic/">
+                        <FontAwesomeIcon icon={faLinkedin} className="icons" />
+                    </a>
+                </div>
+
+            </footer>
         </>
     );
 }
