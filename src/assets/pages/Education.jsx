8db6cc05ca696f2a83b49e807995f121dd77848a
angemiJR <111733781+angemiJR@users.noreply.github.com>
2025-03-25 12:54:59 +0100

    mobile second part

--- src/assets/pages/Education.jsx
+++ src/assets/pages/Education.jsx
@@ -1,105 +1,100 @@
+import { useEffect, useState } from 'react';
 import '../styles/Education.css';
 import '../styles/Common.css';
+
 import Skills_GE_bullet from '../pictures/Skills_GE_bullet.svg';
 import Skills_FD_bullet from '../pictures/Skills_FD_bullet.svg';
 import Skills_SM_bullet from '../pictures/Skills_SM_bullet.svg';
 
 import Chapter from '../components/Chapter_right.jsx';
-import picture1 from '../pictures/Education.svg';
+import pictureDesktop from '../pictures/Education.svg';
+import pictureMobile from '../pictures/Education_M.svg';
 
 const chapterText = "Education";
-function Education() {
-    return (
 
+function Education() {
+    const [isMobile, setIsMobile] = useState(window.innerWidth < 767);
+
+    useEffect(() => {
+        const handleResize = () => setIsMobile(window.innerWidth < 767);
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    return (
         <>
             <div className='chapter_right'>
-
                 <Chapter text={chapterText} />
-
             </div>
+
             <div className='container4'>
-                <div className="box">
+                {isMobile ? (
+                    <img src={pictureMobile} alt="education" id="education_img" />
+                ) : (
+                    <div className="box">
+                        <img src={pictureDesktop} alt="education" id="education_img" />
 
-                    <img src={picture1} alt="bullet" id='education_img' />
-
-                    <div className="div01">
-                        <div className="text_box">
-                            <h6>Bachelor’s degree with honors in graphic
-                                engineering and design</h6>
-                            <p>Department of Graphic Engineering and Design </p>
-                            <p>Faculty of Technical Science in Novi Sad</p>
-                            <p>October 2009 – October 2013</p>
+                        <div className="div01">
+                            <div className="text_box">
+                                <h6>Bachelor’s degree with honors in graphic engineering and design</h6>
+                                <p>Department of Graphic Engineering and Design </p>
+                                <p>Faculty of Technical Science in Novi Sad</p>
+                                <p>October 2009 – October 2013</p>
+                            </div>
+                            <img src={Skills_GE_bullet} alt="bullet" className='bullets' />
                         </div>
 
-                        <img src={Skills_GE_bullet} alt="bullet" className='bullets' />
-                    </div>
-
-                    <div className="div02">
-                        <div className="text_box">
-                            <h6>Master’s degree in graphic
-                                engineering and design</h6>
-                            <p>Department of Graphic Engineering and Design </p>
-                            <p>Faculty of Technical Science in Novi Sad</p>
-                            <p>October 2013 – October 2014  </p>
+                        <div className="div02">
+                            <div className="text_box">
+                                <h6>Master’s degree in graphic engineering and design</h6>
+                                <p>Department of Graphic Engineering and Design </p>
+                                <p>Faculty of Technical Science in Novi Sad</p>
+                                <p>October 2013 – October 2014</p>
+                            </div>
+                            <img src={Skills_GE_bullet} alt="bullet" className='bullets' />
                         </div>
 
-                        <img src={Skills_GE_bullet} alt="bullet" className='bullets' />
-                    </div>
-
-                    <div className="div03">
-                        <div className="text_box">
-                            <h6>Front-end developer</h6>
-                            <p>Sundsgårdens folkhögskola, Helsingborg,
-                            </p>
-                            <p>August 2024 - January 2025</p>
+                        <div className="div03">
+                            <div className="text_box">
+                                <h6>Front-end developer</h6>
+                                <p>Sundsgårdens folkhögskola, Helsingborg</p>
+                                <p>August 2024 - January 2025</p>
+                            </div>
+                            <img src={Skills_FD_bullet} alt="bullet" className='bullets' />
                         </div>
 
-                        <img src={Skills_FD_bullet} alt="bullet" className='bullets' />
-                    </div>
-
-
-                    <div className="div04">
-                        <img src={Skills_FD_bullet} alt="bullet" className='bullets' />
-                        <div className="text_box">
-                            <h6>Basics of Programming  </h6>
-                            <h6>(in C# and .NET framework)  </h6>
-                            <p>SmartInIT Code Academy, Novi Sad</p>
-                            <p>March 2018</p>
+                        <div className="div04">
+                            <img src={Skills_FD_bullet} alt="bullet" className='bullets' />
+                            <div className="text_box">
+                                <h6>Basics of Programming</h6>
+                                <h6>(in C# and .NET framework)</h6>
+                                <p>SmartInIT Code Academy, Novi Sad</p>
+                                <p>March 2018</p>
+                            </div>
                         </div>
 
-                    </div>
-
-
-                    <div className="div05">
-                        <img src={Skills_SM_bullet} alt="bullet" className='bullets' />
-                        <div className="text_box">
-                            <h6>Scrum Fundamentals Certified (SFC)</h6>
-                            <p>SCRUMstudy - Accreditation Body for
-                                Scrum and Agile, </p>
-                            <p>March 2022</p>
+                        <div className="div05">
+                            <img src={Skills_SM_bullet} alt="bullet" className='bullets' />
+                            <div className="text_box">
+                                <h6>Scrum Fundamentals Certified (SFC)</h6>
+                                <p>SCRUMstudy - Accreditation Body for Scrum and Agile</p>
+                                <p>March 2022</p>
+                            </div>
                         </div>
 
-                    </div>
-
-
-
-                    <div className="div06">
-                        <img src={Skills_SM_bullet} alt="bullet" className='bullets' />
-                        <div className="text_box">
-                            <h6>Professional Scrum Master™ I (PSM I)  </h6>
-                            <p>Scrum.org,  </p>
-                            <p>August 2022 </p>
+                        <div className="div06">
+                            <img src={Skills_SM_bullet} alt="bullet" className='bullets' />
+                            <div className="text_box">
+                                <h6>Professional Scrum Master™ I (PSM I)</h6>
+                                <p>Scrum.org</p>
+                                <p>August 2022</p>
+                            </div>
                         </div>
-
                     </div>
-
-
-                </div>
-
+                )}
             </div>
-
         </>
     );
 }
 
-export default Education; // Ensure you have this default export
+export default Education;
--- src/assets/pages/Skills.jsx
+++ src/assets/pages/Skills.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import '../styles/Skills.css';
 import '../styles/Common.css';
 
@@ -10,177 +11,119 @@ import Skills_FD_bullet from '../pictures/Skills_FD_bullet.svg';
 import Skills_SM_bullet from '../pictures/Skills_SM_bullet.svg';
 import Skills_GE_bullet from '../pictures/Skills_GE_bullet.svg';
 
-
 const chapterText = "Skills";
+
 function Skills() {
+    const [isMobile, setIsMobile] = useState(window.innerWidth < 767);
+
+    useEffect(() => {
+        const handleResize = () => setIsMobile(window.innerWidth < 767);
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
     return (
         <>
             <div className='chapter_left'>
-
                 <Chapter text={chapterText} />
-
             </div>
+
             <div className='container3'>
+
+                {/* FIRST WHOLE */}
                 <div className="whole">
                     <div className="div2">
                         <img src={Skills_SM} alt="Scrum master logo" className='image' />
                     </div>
                     <div className="grid_container">
-
                         <div className="div3"></div>
                         <div className="div3"></div>
-                        <div className="div4">
-                            <img src={Skills_SM_bullet} alt="Scrum master bullet" className='bullet' />
-                        </div>
-                        <div className="div5">
-                            <p>Proficient in <b>Scrum, Agile </b>methodologies, and frameworks like <b>SAFe</b>
-                            </p>
-                        </div>
-                        <div className="div6">
-                            <img src={Skills_SM_bullet} alt="Scrum master bullet" className='bullet' />
-                        </div>
-                        <div className="div7">
-                            <p>Skilled in team <b>facilitation</b>, <b>leadership</b>, and <b>conflict resolution</b>
-                            </p>
-                        </div>
-                        <div className="div8">
-                            <img src={Skills_SM_bullet} alt="Scrum master bullet" className='bullet' />
-                        </div>
-                        <div className="div9">
-                            <p>Effective <b>problem-solving</b> and removal of blockers
-
-                            </p>
-                        </div>
-                        <div className="div10">
-                            <img src={Skills_SM_bullet} alt="Scrum master bullet" className='bullet' />
-                        </div>
-                        <div className="div11">
-                            <p>Experienced in using tools like <b>JIRA</b>, <b>Confluence</b>
-                            </p>
-                        </div>
-                        <div className="div12">
-                            <img src={Skills_SM_bullet} alt="Scrum master bullet" className='bullet' />
-                        </div>
-                        <div className="div13">
-                            <p>High emotional intelligence for team dynamics and <b>fostering collaboration</b>
-
-                            </p>
-                        </div>
+                        <div className="div4"><img src={Skills_SM_bullet} alt="" className='bullet' /></div>
+                        <div className="div5"><p>Proficient in <b>Scrum, Agile</b> methodologies, and frameworks like <b>SAFe</b></p></div>
+                        <div className="div6"><img src={Skills_SM_bullet} alt="" className='bullet' /></div>
+                        <div className="div7"><p>Skilled in team <b>facilitation</b>, <b>leadership</b>, and <b>conflict resolution</b></p></div>
+                        <div className="div8"><img src={Skills_SM_bullet} alt="" className='bullet' /></div>
+                        <div className="div9"><p>Effective <b>problem-solving</b> and removal of blockers</p></div>
+                        <div className="div10"><img src={Skills_SM_bullet} alt="" className='bullet' /></div>
+                        <div className="div11"><p>Experienced in using tools like <b>JIRA</b>, <b>Confluence</b></p></div>
+                        <div className="div12"><img src={Skills_SM_bullet} alt="" className='bullet' /></div>
+                        <div className="div13"><p>High emotional intelligence for team dynamics and <b>fostering collaboration</b></p></div>
                     </div>
                 </div>
+
+                {/* SECOND WHOLE - CONDITIONAL ORDER */}
                 <div className="whole">
-                <div className="grid_container2">
-
-                    <div className="div3"></div>
-                    <div className="div3"></div>
-                    <div className="div4">
-                        <img src={Skills_FD_bullet} alt="Frontend bullet" className='bullet' />
-                    </div>
-                    <div className="div5">
-                        <p>Proficient in <b>HTML5, CSS3, SCSS </b> (Sass), and <b>JavaScript</b>
-
-
-                        </p>
-                    </div>
-                    <div className="div6">
-                        <img src={Skills_FD_bullet} alt="Frontend bullet" className='bullet' />
-                    </div>
-                    <div className="div7">
-                        <p>Experienced in modern frameworks such as <b> React</b>
-
-
-                        </p>
-                    </div>
-                    <div className="div8">
-                        <img src={Skills_FD_bullet} alt="Frontend bullet" className='bullet' />
-                    </div>
-                    <div className="div9">
-                        <p>Skilled in UI/UX design using <b>Figma</b> and modern design libraries
-
-                        </p>
-                    </div>
-                    <div className="div10">
-                        <img src={Skills_FD_bullet} alt="Frontend bullet bullet" className='bullet' />
-                    </div>
-                    <div className="div11">
-                        <p>Adept at version control with <b>Git</b> and <b>GitHub</b>
-
-                        </p>
-                    </div>
-                    <div className="div12">
-                        <img src={Skills_FD_bullet} alt="Frontend bullet bullet" className='bullet' />
-                    </div>
-                    <div className="div13">
-                        <p>Strong foundation in responsive and user-centric web development principles
-
-                        </p>
-                    </div>
-                    <div className="div3"></div>
-                    <div className="div3"></div>
-        
-                    
+                    {isMobile ? (
+                        <>
+                            <div className="div2">
+                                <img src={Skills_FD} alt="Front end logo" className='image' />
+                            </div>
+                            <div className="grid_container2">
+                                <div className="div3"></div>
+                                <div className="div3"></div>
+                                <div className="div4"><img src={Skills_FD_bullet} alt="" className='bullet' /></div>
+                                <div className="div5"><p>Proficient in <b>HTML5, CSS3, SCSS</b> (Sass), and <b>JavaScript</b></p></div>
+                                <div className="div6"><img src={Skills_FD_bullet} alt="" className='bullet' /></div>
+                                <div className="div7"><p>Experienced in modern frameworks such as <b>React</b></p></div>
+                                <div className="div8"><img src={Skills_FD_bullet} alt="" className='bullet' /></div>
+                                <div className="div9"><p>Skilled in UI/UX design using <b>Figma</b> and modern design libraries</p></div>
+                                <div className="div10"><img src={Skills_FD_bullet} alt="" className='bullet' /></div>
+                                <div className="div11"><p>Adept at version control with <b>Git</b> and <b>GitHub</b></p></div>
+                                <div className="div12"><img src={Skills_FD_bullet} alt="" className='bullet' /></div>
+                                <div className="div13"><p>Strong foundation in responsive and user-centric web development principles</p></div>
+                                <div className="div3"></div>
+                                <div className="div3"></div>
+                            </div>
+                        </>
+                    ) : (
+                        <>
+                            <div className="grid_container2">
+                                <div className="div3"></div>
+                                <div className="div3"></div>
+                                <div className="div4"><img src={Skills_FD_bullet} alt="" className='bullet' /></div>
+                                <div className="div5"><p>Proficient in <b>HTML5, CSS3, SCSS</b> (Sass), and <b>JavaScript</b></p></div>
+                                <div className="div6"><img src={Skills_FD_bullet} alt="" className='bullet' /></div>
+                                <div className="div7"><p>Experienced in modern frameworks such as <b>React</b></p></div>
+                                <div className="div8"><img src={Skills_FD_bullet} alt="" className='bullet' /></div>
+                                <div className="div9"><p>Skilled in UI/UX design using <b>Figma</b> and modern design libraries</p></div>
+                                <div className="div10"><img src={Skills_FD_bullet} alt="" className='bullet' /></div>
+                                <div className="div11"><p>Adept at version control with <b>Git</b> and <b>GitHub</b></p></div>
+                                <div className="div12"><img src={Skills_FD_bullet} alt="" className='bullet' /></div>
+                                <div className="div13"><p>Strong foundation in responsive and user-centric web development principles</p></div>
+                                <div className="div3"></div>
+                                <div className="div3"></div>
+                            </div>
+                            <div className="div2">
+                                <img src={Skills_FD} alt="Front end logo" className='image' />
+                            </div>
+                        </>
+                    )}
                 </div>
 
-                <div className="div2">
-                        <img src={Skills_FD} alt="Front end logo" className='image' />
-                    </div>
-                </div>
+                {/* THIRD WHOLE */}
                 <div className="whole">
                     <div className="div2">
-                        <img src={Skills_GE} alt="Graphic engeneer logo" className='image' />
+                        <img src={Skills_GE} alt="Graphic engineer logo" className='image' />
                     </div>
                     <div className="grid_container">
                         <div className="div3"></div>
                         <div className="div3"></div>
-                        <div className="div4">
-                            <img src={Skills_GE_bullet} alt="Graphic engeneer bullet" className='bullet' />
-                        </div>
-                        <div className="div5">
-                            <p>Strong <b>negotiation skills</b> for client and supplier interactions
-
-                            </p>
-                        </div>
-                        <div className="div6">
-                            <img src={Skills_GE_bullet} alt="Graphic engeneer bullet" className='bullet' />
-                        </div>
-                        <div className="div7">
-                            <p>Expertise in client <b>collaboration</b> and delivering customized solutions
-
-                            </p>
-                        </div>
-                        <div className="div8">
-                            <img src={Skills_GE_bullet} alt="Graphic engeneer bullet" className='bullet' />
-                        </div>
-                        <div className="div9">
-                            <p>Proficient in using <b>Adobe InDesign</b> and <b>Illustrator</b> for graphic production
-
-
-                            </p>
-                        </div>
-                        <div className="div10">
-                            <img src={Skills_GE_bullet} alt="Graphic engeneer bullet" className='bullet' />
-                        </div>
-                        <div className="div11">
-                            <p>Adept at fostering a <b>collaborative</b> and <b>supportive</b> learning environment
-
-                            </p>
-                        </div>
-                        <div className="div12">
-                            <img src={Skills_GE_bullet} alt="Graphic engeneer bullet" className='bullet' />
-                        </div>
-                        <div className="div13">
-                            <p>Team player with a <b>leadership</b> role
-
-                            </p>
-                        </div>
+                        <div className="div4"><img src={Skills_GE_bullet} alt="" className='bullet' /></div>
+                        <div className="div5"><p>Strong <b>negotiation skills</b> for client and supplier interactions</p></div>
+                        <div className="div6"><img src={Skills_GE_bullet} alt="" className='bullet' /></div>
+                        <div className="div7"><p>Expertise in client <b>collaboration</b> and delivering customized solutions</p></div>
+                        <div className="div8"><img src={Skills_GE_bullet} alt="" className='bullet' /></div>
+                        <div className="div9"><p>Proficient in using <b>Adobe InDesign</b> and <b>Illustrator</b> for graphic production</p></div>
+                        <div className="div10"><img src={Skills_GE_bullet} alt="" className='bullet' /></div>
+                        <div className="div11"><p>Adept at fostering a <b>collaborative</b> and <b>supportive</b> learning environment</p></div>
+                        <div className="div12"><img src={Skills_GE_bullet} alt="" className='bullet' /></div>
+                        <div className="div13"><p>Team player with a <b>leadership</b> role</p></div>
                     </div>
                 </div>
+
             </div>
-
-
-
         </>
     );
 }
 
-export default Skills; // Ensure you have this default export
+export default Skills;
